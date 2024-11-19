1456bffd8aa202c270ae0399f1e85f856835d6e8
ZhangHaishuo <1415522673@qq.com>
2024-11-19 22:07:24 +0800

    fix:DDPG

--- 5. DDPG/main.py
+++ 5. DDPG/main.py
@@ -8,11 +8,6 @@ import matplotlib.pyplot as plt
 
 from agent import Agent
 
-environment = gym.make(id='Pendulum-v1')
-STATE_NUM = environment.observation_space.shape[0]
-ACTION_NUM = environment.action_space.shape[0]
-
-agent = Agent(STATE_NUM, ACTION_NUM)
 
 # Hyperparameters
 NUM_EPISODE = 100
@@ -21,52 +16,60 @@ EPSILON_START = 1
 EPSILON_END = 0.02
 EPSILON_DECAY = NUM_EPISODE*NUM_STEP/2
 
-# process data
-reward_buffer = np.empty(shape=NUM_EPISODE)
-reward_list = []
 
-for episode_i in range(NUM_EPISODE):
-    state, info = environment.reset()
-    episode_reward = 0
+if __name__ == '__main__':
+    environment = gym.make(id='Pendulum-v1')
+    STATE_NUM = environment.observation_space.shape[0]
+    ACTION_NUM = environment.action_space.shape[0]
 
-    for step_i in range(NUM_STEP):
-        # get action by epsilon_greedy method
-        epsilon = np.interp(x=episode_i*NUM_STEP+step_i, xp=[0, EPSILON_DECAY], fp=[EPSILON_START, EPSILON_END])
-        random_sample = random.random()
-        if random_sample <= epsilon:
-            action = np.random.uniform(low=-2, high=2, size=ACTION_NUM)
-        else:
-            action = agent.get_action(state)
+    agent = Agent(STATE_NUM, ACTION_NUM)
 
-        # perform the action
-        next_state, reward, terminated, truncated, info = environment.step(action)
+    # process data
+    reward_buffer = np.empty(shape=NUM_EPISODE)
+    reward_list = []
 
-        # store experience into replayBuffer
-        agent.replay_buffer.add_experience(state, action, reward, next_state, terminated)
+    for episode_i in range(NUM_EPISODE):
+        state, info = environment.reset()
+        episode_reward = 0
 
-        state = next_state
-        episode_reward += reward
+        for step_i in range(NUM_STEP):
+            # get action by epsilon_greedy method
+            epsilon = np.interp(x=episode_i*NUM_STEP+step_i, xp=[0, EPSILON_DECAY], fp=[EPSILON_START, EPSILON_END])
+            random_sample = random.random()
+            if random_sample <= epsilon:
+                action = np.random.uniform(low=-2, high=2, size=ACTION_NUM)
+            else:
+                action = agent.get_action(state)
 
-        agent.update()
-        if terminated:
-            break
+            # perform the action
+            next_state, reward, terminated, truncated, info = environment.step(action)
 
-    reward_buffer[episode_i] = episode_reward
-    reward_list.append(episode_reward)
-    print(f"Episode: {episode_i+1}, Reward: {round(episode_reward, 3)}")
+            # store experience into replayBuffer
+            agent.replay_buffer.add_experience(state, action, reward, next_state, terminated)
 
-agent.plot()
-plt.plot(range(len(reward_list)), reward_list, color='b')
-plt.title('reward')
-plt.xlabel('episode')
-plt.ylabel('reward')
-plt.show()
+            state = next_state
+            episode_reward += reward
 
-# save model
-current_path = os.path.dirname(os.path.realpath(__file__))
-model_path = current_path + '/models/'
-timestamp = time.strftime("%Y%m%d%H%M%S")
-torch.save(agent.actor.state_dict(), model_path + f"ddpg_actor_{timestamp}.pth")
-torch.save(agent.critic.state_dict(), model_path + f"ddpg_critic_{timestamp}.pth")
+            agent.update()
+            if terminated:
+                break
 
-environment.close()
+        reward_buffer[episode_i] = episode_reward
+        reward_list.append(episode_reward)
+        print(f"Episode: {episode_i+1}, Reward: {round(episode_reward, 3)}")
+
+    agent.plot()
+    plt.plot(range(len(reward_list)), reward_list, color='b')
+    plt.title('reward')
+    plt.xlabel('episode')
+    plt.ylabel('reward')
+    plt.show()
+
+    # save model
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    model_path = current_path + '/models/'
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    torch.save(agent.actor.state_dict(), model_path + f"ddpg_actor_{timestamp}.pth")
+    torch.save(agent.critic.state_dict(), model_path + f"ddpg_critic_{timestamp}.pth")
+
+    environment.close()
--- 5. DDPG/test.py
+++ 5. DDPG/test.py
@@ -1,10 +1,7 @@
 import os.path
-
-import numpy as np
 import torch
 import torch.nn as nn
 import gym
-import pygame
 
 
 class Actor(nn.Module):
@@ -21,24 +18,11 @@ class Actor(nn.Module):
         return x
 
 
-def process_frame(frame, width, height):
-    frame = np.transpose(frame, (1, 0, 2))
-    frame = pygame.surfarray.make_surface(frame)
-    return pygame.transform.scale(frame, (width, height))
-
-
 if __name__ == '__main__':
-
-    # init pygame
-    pygame.init()
-    width, height = 600, 600
-    screen = pygame.display.set_mode((width, height))
-    clock = pygame.time.Clock()
-
     current_path = os.path.dirname(os.path.realpath(__file__))
     model_path = current_path + '/models/ddpg_actor_20240709125035.pth'
 
-    environment = gym.make(id='Pendulum-v1', render_mode="rgb_array")
+    environment = gym.make(id='Pendulum-v1', render_mode="human")
     state_num = environment.observation_space.shape[0]
     action_num = environment.action_space.shape[0]
     actor = Actor(state_num, action_num)
@@ -54,12 +38,6 @@ if __name__ == '__main__':
             state = next_state
             episode_reward += reward
 
-            frame = process_frame(environment.render(), width, height)
-            screen.blit(frame, (0, 0))
-            pygame.display.flip()
-            clock.tick(60)
-
         print(f"Episode: {episode_i + 1}, Reward: {round(episode_reward, 3)}")
 
-    pygame.quit()
     environment.close()
